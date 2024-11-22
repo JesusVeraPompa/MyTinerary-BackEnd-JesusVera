27cd0b32d0a632278e6f787d817844ce150c6b3a
JesusVeraPompa <merkapompa@gmail.com>
2024-11-22 18:36:12 -0500

    7.6 Corrigiendo Errores

--- schemas/users/createSchema.js
+++ schemas/users/createSchema.js
@@ -1,23 +1,27 @@
 import joi from "joi-oid";
 
 const schema = joi.object({
-    name: joi.string().required().alphanum().messages({
+    name: joi.string().pattern(/^[a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/).required().min(3).alphanum().messages({
         'string.base':'Do not use numbers',
-        'string.alphanum':'Do not use special characters'
+        'string.pattern.base':'"name" Do not use numbers and use special characters',
+        'string.min':'"name" Must be a minimum of 3 characters'
     }),
-    surname: joi.string().alphanum().messages({
+    surname: joi.string().pattern(/^[a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/).alphanum().min(3).messages({
         'string.base':'Do not use numbers',
-        'string.alphanum':'Do not use special characters'
+        'string.pattern.base':'"surname" Do not use numbers and use special characters',
+        'string.min':'"name" Must be a minimum of 3 characters'
     }),
     email: joi.string().required(),
     photo: joi.string().required(),
-    country: joi.string().alphanum().messages({
+    country: joi.string().required().alphanum().min(3).messages({
         'string.base':'Do not use numbers',
-        'string.alphanum':'Do not use special characters'
+        'string.alphanum':'Do not use special characters',
+        'string.min':'"name" Must be a minimum of 3 characters'
     }),
-    role: joi.string().required().alphanum().messages({
+    role: joi.string().required().min(3).alphanum().messages({
         'string.base':'Do not use numbers',
-        'string.alphanum':'Do not use special characters'
+        'string.alphanum':'Do not use special characters',
+        'string.min':'"name" Must be a minimum of 3 characters'
     }),
     password: joi.string().required()
 })
